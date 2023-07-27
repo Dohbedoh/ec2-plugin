1c97f6d348186dc466465e33df99cc69db1e2090
Allan Burdajewicz <aburdajewicz@cloudbees.com>
2023-07-27 10:06:31 +1000

    Progress

--- src/main/java/hudson/plugins/ec2/util/AmazonEC2FactoryImpl.java
+++ src/main/java/hudson/plugins/ec2/util/AmazonEC2FactoryImpl.java
@@ -1,21 +1,21 @@
 package hudson.plugins.ec2.util;
 
-import java.net.URL;
-
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.services.ec2.AmazonEC2;
-import com.amazonaws.services.ec2.AmazonEC2Client;
-
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 import hudson.Extension;
 import hudson.plugins.ec2.EC2Cloud;
+
+import java.net.URL;
 
 @Extension
 public class AmazonEC2FactoryImpl implements AmazonEC2Factory {
 
     @Override
     public AmazonEC2 connect(AWSCredentialsProvider credentialsProvider, URL ec2Endpoint) {
-        AmazonEC2 client = new AmazonEC2Client(credentialsProvider, EC2Cloud.createClientConfiguration(ec2Endpoint.getHost()));
-        client.setEndpoint(ec2Endpoint.toString());
-        return client;
+        return AmazonEC2ClientBuilder.standard()
+            .withCredentials(credentialsProvider)
+            .withClientConfiguration(EC2Cloud.createClientConfiguration(ec2Endpoint.getHost()))
+            .build();
     }
 }
